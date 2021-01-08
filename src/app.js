6ed6bab51705600521e6c2609075a2614f76d625
aniledev <elinamcgill@gmail.com>
2021-01-07 21:54:36 -0500

    FEAT: validate genres query

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const cors = require("cors");
 const winston = require("winston");
 const { NODE_ENV, PORT } = require("./config");
+const MOVIEDEX = require("./movies-data-small.json");
 
 const app = express();
 
@@ -29,12 +30,57 @@ if (NODE_ENV !== "production") {
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+// add a validation function here for the authorization token
 app.use(express.json());
 
 //ROUTES
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
+const handleGetMovies = (req, res, next) => {
+  // read the req.query object; provide default values for the ones that are required
+  const { genre = "", country = "", avg_vote } = req.query;
+
+  //validate genre if there is one; genre must be one of the valid types
+  const validGenres = [
+    "Animation",
+    "Drama",
+    "Romantic",
+    "Comedy",
+    "Crime",
+    "Thriller",
+    "Adventure",
+    "Documentary",
+    "Horror",
+    "Action",
+    "Western",
+    "Spy",
+    "Final Embrace",
+    "History",
+    "Biography",
+    "Musical",
+    "Fantasy",
+    "War",
+    "Grotesque",
+  ];
+
+  // if there is a genre, it must also be validated
+  if (genre) {
+    let response = MOVIEDEX;
+
+    if (!validGenres.toString().includes(genre)) {
+      // if genres is not one of the validgenres, then return a status of 400
+      return res
+        .status(400)
+        .send(
+          "Genre must either be Action, Puzzle, Strategy, Casual, Arcade or Card"
+        );
+    }
+  }
+
+  // use the filter method to filter out searches that dont include country string
+  // for avg vote, use the filter method to filter out searches that dont include country string
+
+  res.json(response);
+};
+app.get("/movie", handleGetMovies);
 
 // CATCH ANY THROWN ERRORS AND THEN DEFINE THE ERROR AND KEEP THE APPLICATION RUNNING;
 //STILL MIDDLEWARE
